648dae6a44b0773656591d59ed6a65233e035c2d
Rokas-B-Tester <rokasbucevicius@gmail.com>
2022-10-15 19:49:48 +0300

    Added Override on setUp method

--- src/test/java/lt/rokas/pom/pages/biofitus/BiofitusHomePage.java
+++ src/test/java/lt/rokas/pom/pages/biofitus/BiofitusHomePage.java
@@ -1,5 +1,10 @@
 package lt.rokas.pom.pages.biofitus;
 
+import lt.rokas.pom.pages.Common;
+
 public class BiofitusHomePage {
 
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
 }
--- src/test/java/lt/rokas/pom/tests/biofitus/BiofitusHomePageTest.java
+++ src/test/java/lt/rokas/pom/tests/biofitus/BiofitusHomePageTest.java
@@ -1,6 +1,15 @@
 package lt.rokas.pom.tests.biofitus;
 
-public class BiofitusHomePageTest {
+import lt.rokas.pom.pages.biofitus.BiofitusHomePage;
+import lt.rokas.pom.tests.TestBase;
+import org.testng.annotations.BeforeMethod;
 
+public class BiofitusHomePageTest extends TestBase {
 
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        BiofitusHomePage.open("https://biofitus.lt/");
+    }
 }
