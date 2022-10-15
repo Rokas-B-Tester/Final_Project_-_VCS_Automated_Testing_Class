dcf9cffe590c90b244ae55944a4b1633f4cf5ed1
Rokas-B-Tester <rokasbucevicius@gmail.com>
2022-10-15 19:02:34 +0300

    Added Before & After Method to TestBase Class

--- src/test/java/lt/rokas/pom/tests/TestBase.java
+++ src/test/java/lt/rokas/pom/tests/TestBase.java
@@ -1,6 +1,20 @@
 package lt.rokas.pom.tests;
 
+import lt.rokas.pom.pages.Common;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class TestBase {
 
+    @BeforeMethod
+    public void setUp(){
+        Common.setUpDriver();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        Common.closeDriver();
+
+    }
 
 }
