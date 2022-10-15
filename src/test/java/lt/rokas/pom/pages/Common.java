fbda891c6bd233e4273ed032efedc405b8da4915
Rokas-B-Tester <rokasbucevicius@gmail.com>
2022-10-15 18:55:46 +0300

    Added Driver Methods to Common Class
    Created setUpDriver, openUrl & closeDriver

--- src/test/java/lt/rokas/pom/pages/Common.java
+++ src/test/java/lt/rokas/pom/pages/Common.java
@@ -1,5 +1,22 @@
 package lt.rokas.pom.pages;
 
+import lt.rokas.pom.utils.Driver;
+
 public class Common {
+
+    public static void setUpDriver() {
+        Driver.setDriver();
+
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+
+    }
+
+    public static void closeDriver() {
+        Driver.close();
+
+    }
 
 }
