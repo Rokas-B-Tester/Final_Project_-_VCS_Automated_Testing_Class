a267526ecb98bd564ba790e22d0e8d63b002d411
Rokas-B-Tester <rokasbucevicius@gmail.com>
2022-10-15 19:22:13 +0300

    Added TestListener to TestBase

--- src/test/java/lt/rokas/pom/tests/TestBase.java
+++ src/test/java/lt/rokas/pom/tests/TestBase.java
@@ -1,9 +1,12 @@
 package lt.rokas.pom.tests;
 
 import lt.rokas.pom.pages.Common;
+import lt.rokas.pom.utils.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public class TestBase {
 
     @BeforeMethod
